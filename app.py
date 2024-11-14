94cb7214abbaeb1a8e612c896c725213e96526db
dineshsuthar03 <dineshsutharinfo@gmail.com>
2024-11-14 21:39:54 +0530

    added users

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/user')
+def home():
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5002)
